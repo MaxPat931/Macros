c7a3142eb2f8d67b5bee09a362daf34238ad4941
MaxPat931 <86370342+MaxPat931@users.noreply.github.com>
2022-02-28 17:34:19 -0800

    v2
    GM whisper will fire first, capturing targeted creature, creature type, skill type, and DC

--- HamundsAppraisal.js
+++ HamundsAppraisal.js
@@ -1,7 +1,8 @@
 /// This macro is supposed to be used with Hammonds Harvesting Guide
-/// It provides a drop down to select which type of Appraisal check you will be doing
-/// And roll according to the Adv/Normal/Disadv buttons
-/// It will also send a whisper to the DM for the DC of the targeted monster
+/// It will send a whisper to the DM for the Targeted Creature, the Creature Type, the associated skill to appraise it, and the DC of the check
+/// It provides a drop down to select which type of Appraisal check the player will be doing
+/// And roll according to the Adv/Normal/Disadv buttons selected
+
 
 if (game.user.targets.size !== 1)
     ui.notifications.warn(`Please target one token to appraise.`)
@@ -15,6 +16,33 @@ const med = actor.data.data.skills.med.prof;
 const nat = actor.data.data.skills.nat.prof;
 const [target] = game.user.targets;
 const targetDc = target.actor.data.data.details.cr;
+const targetType = target.actor.data.data.details.type.value;
+
+
+const checkTypes = {
+    beast: 'Nature',
+    dragon: 'Nature',
+    giant: 'Medicine',
+    monstrosity: 'Nature',
+    plant: 'Nature',
+    humanoid: 'Medicine',
+    celestial: 'Arcana',
+    fiend: 'Arcana',
+    undead: 'Arcana',
+    aberration: 'Arcana',
+    construct: 'Invstigation',
+    elemental: 'Arcana',
+    fey: 'Arcana',
+    ooze: 'Investigation'
+};
+
+let checkSkill = checkTypes[targetType]
+
+
+ChatMessage.create({
+    content: `<b>Appraisal of:</b> ${target.data.name} <b>Type:</b> ${targetType} <p><b>Appraisal Skill:</b> ${checkSkill}  <b>DC:</b> [[floor(8+${targetDc})]]</p>`,
+    whisper: ChatMessage.getWhisperRecipients('GM'),
+  })
 
 let d = new Dialog({
     title: 'Appraisal Check',
@@ -33,35 +61,21 @@ let d = new Dialog({
     `,
     buttons: {
       advantage:{
-        icon: '<i class="fas fa-check"></i>',
           label: 'Advantage',
           callback: (html) => {
-            new Roll(`2d20kh+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` }),
-             ChatMessage.create({
-                content: `${target.data.name} DC: [[floor(8+${targetDc})]]`,
-                whisper: ChatMessage.getWhisperRecipients('GM'),
-              })     
+            new Roll(`2d20kh+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` })
         }
       },
       normal: {
         label: 'Normal',
         callback: (html) => {
-            new Roll(`1d20+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` }),
-            ChatMessage.create({
-                content: `${target.data.name} DC: [[floor(8+${targetDc})]]`,
-                whisper: ChatMessage.getWhisperRecipients('GM'),
-              })     
+            new Roll(`1d20+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` })
         }
       },
       disadvantage: {
-        icon: '<i class="fas fa-times"></i>',
         label: 'Disadvantage',
         callback: (html) => {
-            new Roll(`2d20kl+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` }),
-            ChatMessage.create({
-                content: `${target.data.name} DC: [[floor(8+${targetDc})]]`,
-                whisper: ChatMessage.getWhisperRecipients('GM'),
-              })     
+            new Roll(`2d20kl+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` })    
         }
       },
     },
