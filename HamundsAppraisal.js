452d2a300dd251d7d497b7a31c15756c56c970b9
Kenneth B <kennethzhell@gmail.com>
2023-05-31 21:17:14 +0200

    update hamund

--- HamundsAppraisal.js
+++ HamundsAppraisal.js
@@ -3,85 +3,32 @@
 /// It provides a drop down to select which type of Appraisal check the player will be doing
 /// And roll according to the Adv/Normal/Disadv buttons selected
 
-
-if (game.user.targets.size !== 1)
-    ui.notifications.warn(`Please target one token to appraise.`)
-else{
-
-let actors = canvas.tokens.controlled.map(({ actor }) => actor);
-const intMod = actor.data.data.abilities.int.mod;
-const arc = actor.data.data.skills.arc.prof;
-const inv = actor.data.data.skills.inv.prof;
-const med = actor.data.data.skills.med.prof;
-const nat = actor.data.data.skills.nat.prof;
-const [target] = game.user.targets;
-const targetDc = target.actor.data.data.details.cr;
-const targetType = target.actor.data.data.details.type.value;
-
-
-const checkTypes = {
-    beast: 'Nature',
-    dragon: 'Nature',
-    giant: 'Medicine',
-    monstrosity: 'Nature',
-    plant: 'Nature',
-    humanoid: 'Medicine',
-    celestial: 'Arcana',
-    fiend: 'Arcana',
-    undead: 'Arcana',
-    aberration: 'Arcana',
-    construct: 'Invstigation',
-    elemental: 'Arcana',
-    fey: 'Arcana',
-    ooze: 'Investigation'
-};
-
-let checkSkill = checkTypes[targetType]
-
+if (game.user.targets.size !== 1) return ui.notifications.warn("Please target exactly one token to appraise.");
+const target = game.user.targets.first();
+const tsd = target.actor.system.details;
+const checkSkill = {
+  beast: "nat",
+  dragon: "nat",
+  giant: "med",
+  monstrosity: "nat",
+  plant: "nat",
+  humanoid: "med",
+  celestial: "arc",
+  fiend: "arc",
+  undead: "arc",
+  aberration: "arc",
+  construct: "inv",
+  elemental: "arc",
+  fey: "arc",
+  ooze: "inv"
+}[tsd.type.value];
+if (!checkSkill) return ui.notifications.warn("Target actor has no creature type.");
 
 ChatMessage.create({
-    content: `<b>Appraisal of:</b> ${target.data.name} <b>Type:</b> ${targetType} <p><b>Appraisal Skill:</b> ${checkSkill}  <b>DC:</b> [[floor(8+${targetDc})]]</p>`,
-    whisper: ChatMessage.getWhisperRecipients('GM'),
-  })
+  content: `
+  <b>Appraisal of:</b> ${target.document.name} <b>Type:</b> ${game.i18n.localize(CONFIG.DND5E.creatureTypes[tsd.type.value])}
+  <p><b>Appraisal Skill:</b> ${CONFIG.DND5E.skills[checkSkill].label} <b>DC:</b> [[floor(8+${tsd.cr})]]</p>`,
+  whisper: ChatMessage.getWhisperRecipients('GM'),
+});
 
-let d = new Dialog({
-    title: 'Appraisal Check',
-    content: `
-      <form class="flexcol">
-        <div class="form-group">
-          <label for="exampleSelect">Select Appraisal Type</label>
-          <select id="exampleSelect">
-            <option value="${arc}">Arcana - Abberation, Celestial, Elemental, Fey, Fiend, Undead</option>
-            <option value="${inv}">Investigation - Construct, Ooze</option>
-            <option value="${med}">Medicine - Giant, Humanoid</option>
-            <option value="${nat}">Nature - Beast, Dragon, Monstrosity, Plant</option>
-          </select>
-        </div>
-      </form>
-    `,
-    buttons: {
-      advantage:{
-          label: 'Advantage',
-          callback: (html) => {
-            new Roll(`2d20kh+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` })
-        }
-      },
-      normal: {
-        label: 'Normal',
-        callback: (html) => {
-            new Roll(`1d20+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` })
-        }
-      },
-      disadvantage: {
-        label: 'Disadvantage',
-        callback: (html) => {
-            new Roll(`2d20kl+${intMod}+${html.find("#exampleSelect")[0].value}`).toMessage({ flavor: `${actor.data.name} appraises the ${target.data.name}` })    
-        }
-      },
-    },
-    default: 'yes',
-    close: () => {
-      console.log('Dialog Closed');
-    }
-  }).render(true)
-}
+return actor.rollSkill(checkSkill, {event, targetValue: 8 + tsd.cr});
