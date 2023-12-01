04550a9342e35c077be12a8d72cd3389a30abb5b
MaxPat931 <86370342+MaxPat931@users.noreply.github.com>
2023-12-01 11:30:03 -0800

    Update WS-HideSourceConfig.js

--- WS-HideSourceConfig.js
+++ WS-HideSourceConfig.js
@@ -3,5 +3,6 @@ Hooks.on("renderItemSheet", (sheet, [html]) => {
   if (game.user.isGM) return;
   const unidentified = sheet.item.system.identified === false;
   if (!unidentified) return;
-  html.querySelectorAll(".dnd5e.sheet.item .sheet-header .summary li .config-button").forEach(n => n.remove());
+  html.querySelectorAll(".dnd5e.sheet.item .sheet-header .summary li .config-button").forEach(n => n.remove()); //remove the Source gonfig gear
+  html.querySelectorAll(".dnd5e.sheet.item [data-tab="description"] .item-description .accordion:first-child").forEach(n => n.remove()); //remove the description
 });
