976dcd938148d888284319823a40066830c9003c
MaxPat931 <86370342+MaxPat931@users.noreply.github.com>
2023-12-05 09:21:24 -0800

    Update WS-HideUnidentified.js

--- WS-HideUnidentified.js
+++ WS-HideUnidentified.js
@@ -3,7 +3,7 @@ Hooks.on("renderItemSheet", (sheet, [html]) => {
   if (game.user.isGM) return;
   const unidentified = sheet.item.system.identified === false;
   if (!unidentified) return;
-  html.querySelectorAll(".dnd5e.sheet.item .sheet-header .summary li .config-button").forEach(n => n.remove()); //remove the Source gonfig gear
+  html.querySelectorAll(".dnd5e.sheet.item .sheet-header .summary li .config-button").forEach(n => n.remove()); //remove the Source config gear
   html.querySelectorAll(`.dnd5e.sheet.item [data-tab="description"] .item-description .accordion:first-child`).forEach(n => n.remove()); //remove the Description
 });
 
@@ -22,7 +22,7 @@ Hooks.on("renderChatMessage", async (chatItem, html) => {
 });
 
 /*
-Hooks.on("renderActorsSheet5eCharacter", (html, options) => {
+Hooks.on("renderActorsSheet5eCharacter", (sheet, html, data) => {
   Something with .item-summary?
 });
 */
