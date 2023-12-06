11a5a3c6d79abce30567b85cb6924511d7e3e7cf
MaxPat931 <86370342+MaxPat931@users.noreply.github.com>
2023-12-06 14:07:43 -0800

    Lumberjack
    Chop Logs
    fix indents

--- WS-HideUnidentified.js
+++ WS-HideUnidentified.js
@@ -1,4 +1,4 @@
-///Hides Source Config and description.value from Players if the item is unidentified
+/// Hides Source Config and description.value from Players if the item is unidentified
 Hooks.on("renderItemSheet", (sheet, [html]) => {
   if (game.user.isGM) return;
   const unidentified = sheet.item.system.identified === false;
@@ -7,18 +7,20 @@ Hooks.on("renderItemSheet", (sheet, [html]) => {
   html.querySelectorAll(`.dnd5e.sheet.item [data-tab="description"] .item-description .accordion:first-child`).forEach(n => n.remove()); //remove the Description
 });
 
-/// If an item is used, and it is Unidentified, display the Chat Description if it exists, otherwise display the Unidentified description if it exists, otherwise display the Description (which is hidden from the player due to the above script)
+/// When an item is used, and it is Unidentified, display the Chat Description if it exists, 
+/// otherwise display the Unidentified description if it exists, 
+/// otherwise display the Description (which is hidden from the player due to the above script)
 Hooks.on("renderChatMessage", async (chatItem, html) => {
-        const itemId = chatItem.flags.dnd5e.use.itemUuid;
-        const itemData = await fromUuid(itemId);
-        const unidentified = itemData.system.identified === false;
-        if (!unidentified) return;
-        const itemDescription = itemData.system.description.value;
-        const unidentifiedDesc = itemData.system.description.unidentified;
-        const chatDesc = itemData.system.description.chat;
-        const description = chatDesc || unidentifiedDesc || itemDescription;
-        const cardContent = html.find(".card-content");
-        cardContent.html(description);
+  const itemId = chatItem.flags.dnd5e.use.itemUuid;
+  const itemData = await fromUuid(itemId);
+  const unidentified = itemData.system.identified === false;
+  if (!unidentified) return;
+  const itemDescription = itemData.system.description.value;
+  const unidentifiedDesc = itemData.system.description.unidentified;
+  const chatDesc = itemData.system.description.chat;
+  const description = chatDesc || unidentifiedDesc || itemDescription;
+  const cardContent = html.find(".card-content");
+  cardContent.html(description);
 });
 
 /*
@@ -33,19 +35,17 @@ Hooks.on("updateItem", identify);
 
 async function identify(item, update, options){
   if (game.user.isGM) {
-    console.log(item.name)
     const identification = update.system?.identified === true;
     if(!identification) return;
     
     const sourceItem = await fromUuid(item.flags.core.sourceId);
-    console.log(sourceItem.name)
     if (item.name === sourceItem.name) return;
 
     new Dialog({
       content: await TextEditor.enrichHTML(`<p>Do you want to replace ${item.name} with @UUID[${item.flags.core.sourceId}]?</p>`),
       title: "Identification",
       buttons: {
-        ok: {label: "OK",
+        yes: {label: "Yes",
           callback: async () => {
             const itemData = game.items.fromCompendium(sourceItem);
             let identified = await item.parent.createEmbeddedDocuments("Item", [itemData]);
@@ -62,4 +62,3 @@ async function identify(item, update, options){
     }).render(true);
   }
 };
-*/
