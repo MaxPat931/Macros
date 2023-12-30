bed357a4712c332086ffea585c40ee52bde05624
MaxPat931 <86370342+MaxPat931@users.noreply.github.com>
2023-12-30 13:41:58 -0800

    Update spellCompendiumSorter.js
    Add Clear option
    Add unlock/lock feature
    Add ui notifications

--- spellCompendiumSorter.js
+++ spellCompendiumSorter.js
@@ -1,53 +1,61 @@
 async function spellCompendiumFolders(method) {
     const key = "dnd5e.spells";
     const pack = game.packs.get(key);
+    const wasLocked = pack.locked;
+    if (wasLocked) await pack.configure({locked:false});
 
     let indexFields, configValue;
-    if (method == "school") {
+    if(method == "school"){
         indexFields = "system.school";
         configValue = "spellSchools";
-    } else if (method == "level") {
+    } else if(method == "level"){
         indexFields = "system.level";
         configValue = "spellLevels"
     }
 
-    const index = await pack.getIndex({ fields: [indexFields] });
+    const index = await pack.getIndex({fields: [indexFields]});
     const config = CONFIG.DND5E[configValue];
 
-    const folderData = Object.values(config).map(v => ({ name: v, type: "Item" }));
-    const folders = await Folder.createDocuments(folderData, { pack: key });
+    const folderData = Object.values(config).map(v => ({name: v, type: "Item"}));
+    const folders = await Folder.createDocuments(folderData, {pack: key});
 
     const updates = [];
-    for (const idx of index) {
-        if (idx.type !== "spell") continue;
+    for(const idx of index) {
+        if(idx.type !== "spell") continue;
         let folderName;
         if (method == "school") {
             folderName = config[idx.system.school];
         } else if (method == "level") {
             folderName = config[idx.system.level];
         }
-        const folder = folders.find(e => e.name === folderName);
-        updates.push({ _id: idx._id, folder: folder.id });
+    const folder = folders.find(e => e.name === folderName);
+    updates.push({_id: idx._id, folder: folder.id});
     }
-    await Item.updateDocuments(updates, { pack: key });
+    await Item.updateDocuments(updates, {pack: key});
+    if (wasLocked) await pack.configure({locked:true});
+    await ui.notifications.warn(`${pack.metadata.label} ${method} Folders Created`)
 }
 
 async function clearCompendiumFolders() {
     const key = "dnd5e.spells";
     const pack = game.packs.get(key);
-    if (pack.folders.size > 0) {
+    const wasLocked = pack.locked;
+    if (wasLocked) await pack.configure({locked:false});
+    if(pack.folders.size > 0){
         const index = await pack.getIndex();
         const updates = [];
-        for (const idx of index) {
-            updates.push({ _id: idx._id, folder: null });
+        for(const idx of index) {
+            updates.push({_id: idx._id, folder: null});
         }
-        await Item.updateDocuments(updates, { pack: key });
+        await Item.updateDocuments(updates, {pack: key});
 
         const packFolders = pack.folders
         const folderIds = packFolders.map(folder => folder.id);
 
-        await Folder.deleteDocuments(folderIds, { pack: key });
-    }
+        await Folder.deleteDocuments(folderIds, {pack: key});
+        }
+    if (wasLocked) await pack.configure({locked:true});
+    await ui.notifications.warn(`${pack.metadata.label} Folders Cleared`)
 }
 
 new Dialog({
@@ -57,15 +65,15 @@ new Dialog({
         level: {
             label: "Spell Level",
             callback: async () => {
-                await clearCompendiumFolders();
-                await spellCompendiumFolders("level");
+            await clearCompendiumFolders();
+            await spellCompendiumFolders("level");
             }
-        },
+            },
         school: {
             label: "Spell School",
             callback: async () => {
-                await clearCompendiumFolders();
-                await spellCompendiumFolders("school");
+            await clearCompendiumFolders();
+            await spellCompendiumFolders("school");
             }
         },
         clear: {
