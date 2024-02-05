1bb037045c49e0c09de19433550e23640a6ed955
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2024-02-05 12:30:58 -0800

    Update HideUnidentify3.js

--- WorldScripts/HideUnidentify3.js
+++ WorldScripts/HideUnidentify3.js
@@ -1,3 +1,5 @@
+// Prevent Players from Identifying their items by hiding the buttons from them, only the GM will be able to Identify them
+// Remove Identify button at top of Item Sheet
 Hooks.on("renderItemSheet", (sheet, [html]) => {
   if (game.user.isGM) return;
   const unidentified = sheet.item.system.identified === false;
@@ -5,9 +7,13 @@ Hooks.on("renderItemSheet", (sheet, [html]) => {
   html.querySelectorAll(".dnd5e.sheet.item .sheet-header .item-subtitle label:has(input:not([disabled]))").forEach(n => n.remove()); //remove Identified toggle on item
 });
 
-Hooks.on("dnd5e.getItemContextOptions", (item, steve) => {
+// Remove Identify button from Item Context menu on Actor Sheet
+Hooks.on("dnd5e.getItemContextOptions", (item, buttons) => {
     if (game.user.isGM) return;
     const unidentified = item.system.identified === false;
     if (!unidentified) return;
-    steve.pop();
+    const removeUnid = buttons.findIndex(option => option.name === 'DND5E.Identify');
+    if (removeUnid !== -1) {
+        buttons.splice(removeUnid, 1);
+    }
 });
