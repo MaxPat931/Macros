368264c0e33b141fb2b6f8b1528cc2ca850b51aa
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2024-02-05 16:56:11 -0800

    Update HideUnidentify3.js
    ZhellQOL'd

--- WorldScripts/HideUnidentify3.js
+++ WorldScripts/HideUnidentify3.js
@@ -4,7 +4,7 @@ Hooks.on("renderItemSheet", (sheet, [html]) => {
   if (game.user.isGM) return;
   const unidentified = sheet.item.system.identified === false;
   if (!unidentified) return;
-  html.querySelectorAll(".dnd5e.sheet.item .sheet-header .item-subtitle label:has(input:not([disabled]))").forEach(n => n.remove()); //remove Identified toggle on item
+  html.querySelectorAll(".dnd5e.sheet.item .sheet-header .item-subtitle label:has(input:not([disabled]))").forEach(n => n.remove());
 });
 
 // Remove Identify button from Item Context menu on Actor Sheet
@@ -13,7 +13,5 @@ Hooks.on("dnd5e.getItemContextOptions", (item, buttons) => {
     const unidentified = item.system.identified === false;
     if (!unidentified) return;
     const removeUnid = buttons.findIndex(option => option.name === 'DND5E.Identify');
-    if (removeUnid !== -1) {
-        buttons.splice(removeUnid, 1);
-    }
+    buttons.findSplice(e => e.name === "DND5E.Identify");
 });
