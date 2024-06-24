ebde37c50717198ebf87511362ec2c6eccd931b2
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2024-06-24 08:11:04 -0700

    Update PrettierItemRolltable.js

--- PrettierItemRolltable.js
+++ PrettierItemRolltable.js
@@ -1,12 +1,12 @@
 const tableName = game.tables.getName("Table Name Here");
-const rollResult = (await tableName.draw({ displayChat: false })).results[0];
-if (!rollResult) return;
-const packId = rollResult.type === "pack" ? `Compendium.${rollResult.documentCollection}` : `Item`;
-const rollId = rollResult.documentId;
-const item = await fromUuid(`${packId}.${rollId}`);
-
-await ChatMessage.create({
-    content: `
+const rollResults = (await tableName.draw({ displayChat: false })).results;
+if (!rollResults || rollResults.length === 0) return;
+let allContent = [];
+for (let rollResult of rollResults) {
+    const packId = rollResult.type === "pack" ? `Compendium.${rollResult.documentCollection}` : `Item`;
+    const rollId = rollResult.documentId;
+    const item = await fromUuid(`${packId}.${rollId}`);
+    const content = `
         <div class="dnd5e2">
             <div class="fvtt advice">
                 <figure class="icon">
@@ -16,5 +16,11 @@ await ChatMessage.create({
                     @UUID[${item.uuid}] ${item.system.description.value}
                 </article>
             </div>
-        </div>`
+        </div>`;
+    allContent.push(content);
+}
+const combinedContent = allContent.join('');
+await ChatMessage.create({
+    content: combinedContent,
+    speaker: { token: "", actor: "", scene: "", alias: "Roll Table Results" }
 });
