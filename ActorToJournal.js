30b38c7f8cdb7115114329d6d0fe3163a3b5e9b6
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2024-06-26 00:59:55 -0700

    Update ActorToJournal.js

--- ActorToJournal.js
+++ ActorToJournal.js
@@ -17,15 +17,8 @@ const influenceJournal = game.journal.getName(mainJournalName) ?? await JournalE
 
 for (const actor of actorFolder.contents) {
     ///Actor Data
-    /// HP, AC, Init, Speed, Prof, Senses
-    /// Level, Class(es), Hit Die
-    /// Abilities, Saves
-    /// Skills
     /// Profs - Armor, Weapon, Language, Tools
-    /// Cash Money
-    /// Spellcasting
-    /// Biography?
-    console.log(actor.system)
+    //console.log(actor.system)
     const abilities = actor.system.abilities;
     const attributes = actor.system.attributes;
     const skills = actor.system.skills;
@@ -42,7 +35,7 @@ for (const actor of actorFolder.contents) {
             abilityContent += `
             <td>
                 <p style="text-align: center;"><span style="font-family: Modesto Condensed; font-size: 2.5em"><strong>${key.toUpperCase()}</strong> 
-                <br>${ability.value} (${ability.mod})</span></p>
+                <br>${ability.value} (${ability.mod > 0 ? `+${ability.mod}` : `${ability.mod}`})${ability.proficient > 0 ? `<i class="fa-solid fa-circle fa-xs"></i>` : ``}</span></p>
             </td>
         `;
         });
@@ -102,22 +95,27 @@ for (const actor of actorFolder.contents) {
     let actorContent = '';
     actorContent = `
     <img src="${actor.img}" width ="300" style="border: none; float:right">
-    <h1>Level ${actor.system.details.level}  HP: __/${actor.system.attributes.hp.max}</h1>
-    <h2>AC: ${attributes.ac.value} 
-    <span style="font-size: .6em;">
-    ${attributes.ac.calc === "default" ? ` ${armorCalc} (${attributes.ac.equippedArmor?.name}, ${attributes.ac.equippedShield?.name})` : `${armorCalc}`}
-    </span>
-    <br>Hit Die: ___/${attributes.hd.max} - 
+    <h1>Level ${actor.system.details.level}  HP: ____/${actor.system.attributes.hp.max}</h1>
     `;
     if (classesArray.length > 0) {
         classesArray.forEach(hitd => {
-            actorContent += `${hitd.name} ${hitd.system.levels}${hitd.system.hitDice}`;
+            let dieCount = '';
+            for (let i = 0; i < hitd.system.levels; i++) {
+                dieCount += `
+                    <i class="fa-duotone fa-dice-${hitd.system.hitDice}"></i>
+                `;
+            }
+            actorContent += `<h3>${hitd.name} ${hitd.system.hitDice} ${dieCount}</h3>`;
         });
     }
 
     actorContent += `
+    <h2>AC: ${attributes.ac.value} 
+    <span style="font-size: .6em;">
+    ${attributes.ac.calc === "default" ? ` ${armorCalc} (${attributes.ac.equippedArmor?.name}, ${attributes.ac.equippedShield?.name})` : `${armorCalc}`}
+    </span>
     <br>Initiative: ${attributes.init.total}
-    <br>Speed: ${movement.walk> 0 ? `Walk ${movement.walk} ${movement.units}` :''}
+    <br>Movement: ${movement.walk> 0 ? `Walk ${movement.walk} ${movement.units}` :''}
     ${movement.climb > 0 ? `Climb ${movement.climb} ${movement.units}` :''}
     ${movement.fly > 0 ? `, Fly ${movement.fly} ${movement.units}` :''}
     ${movement.swim > 0 ? `, Swim ${movement.swim} ${movement.units}` :''}
@@ -148,9 +146,7 @@ for (const actor of actorFolder.contents) {
     <div style="clear:both"></div>`
 
     //Item Data
-    /// Weapons
-    /// Consumables
-    /// Equiptment
+    /// Equipment
     /// Tools
     /// Loot
     /// Containers?!
@@ -162,53 +158,152 @@ for (const actor of actorFolder.contents) {
     /// 
     let itemContent = {
         weapon: [],
-        spell: [],
+        spell: Array.from({ length: 10 }, () => []),
+        spellPact: [],
+        consumable: [],
+
     };
 
     actor.items.forEach(item => {
         const itemName = item.name;
-        //console.log(item)
+        console.log(item.name, item.system)
+        //if (item.system.identified == false) return;
         const desc = item.system.description.value;
-        const itemType = item.type
+        const system = item.system;
+        const itemType = item.type;
+        const labels = item.labels;
+        
         const introContent = `<div class="fvtt advice"><figure class="icon"><img src="${item.img}" class="round"></figure><article>
     `;
 
+    //WEAPONS
         let weaponDesc = '';
         if (itemType === 'weapon') {
-            weaponDesc = `
-            <h2>[[/item ${itemName}]]</h2>
-            ${desc}
-            To Hit: ${item.labels.toHit}
+            
+            weaponDesc = `<span style="font-family: 'Modesto Condensed'; font-size: 2em;">${itemName}${system.quantity > 1 ? `(${system.quantity})` : ``}</span>
+            ${desc}`
+            if (system.consume.type == "attribute"){
+                 weaponDesc += `<br>Consumes: ${system.consume.amount} ${system.consume.target}`;
+                 } else if (system.consume.type == "hitDice"){
+                 weaponDesc += `<br>Consumes: ${system.consume.amount} ${system.consume.target} Hit Die`;
+                 } else if (system.consume.type == "item" || system.consume.type === "ammo" || system.consume.type === "material"){
+                 const conItem = actor.items.get(system.consume.target);
+                 weaponDesc += `<br>Consumes: ${system.consume.amount} ${conItem.name} ${system.consume.type}`;
+                 }
+            weaponDesc+=`
+            <br>To Hit: ${item.labels.toHit}
             <br>Damage: 
         `;
-            if (item.labels.derivedDamage && item.labels.derivedDamage.length > 0) {
-                item.labels.derivedDamage.forEach(damage => {
+            if (labels.derivedDamage && labels.derivedDamage.length > 0) {
+                labels.derivedDamage.forEach(damage => {
                     weaponDesc += `
                     ${damage.label}
                 `;
                 });
             }
-            if (item.labels.range) {
+            if (item.system.damage.versatile){
+                weaponDesc += `<br>Versatile: ${system.damage.versatile}`
+            }
+            if (labels.range) {
                 weaponDesc += `
-                <br>Range: ${item.labels.range}
+                <br>Range: ${labels.range}<br>
             `;
             }
-            if (item.labels.properties && item.labels.properties.length > 0) {
-                item.labels.properties.forEach(prop => {
-                    weaponDesc += `
-                    <br>${prop.label}
+            if (labels.properties && labels.properties.length > 0) {
+                labels.properties.forEach((prop, index) => {
+                    if (index > 0) {
+                    weaponDesc += ', ';
+                    }
+                    weaponDesc += `${prop.label}`;
+                });
+            }
+        }
+        
+    //CONSUMABLES
+        let consumDesc = '';
+        if (itemType === 'consumable') {
+            
+            consumeDesc = `<span style="font-family: 'Modesto Condensed'; font-size: 2em;">${itemName}${system.quantity > 1 ? `(${system.quantity})` : ``}</span>
+            ${desc}
+            ${labels.activation ? `<br>Activation: ${labels.activation}` : ``}
+            ${system.activation.condition ? `<br>Condition: ${system.activation.condition}` : ``}
+            ${labels.range ? `<br>Range: ${labels.range}` : ``}
+            ${labels. save ? `<br>${labels.save} Saving Throw` : ``}
+            ${labels.duration ? `<br>Duration: ${labels.duration}` : ``}
+            ${system.uses.max ? `<br>Uses: ${system.uses.max} ${system.uses.per}` : ``}
+            ${item.labels.toHit ? `<br>To Hit: ${item.labels.toHit}` : ``}
+        `;
+            if (labels.derivedDamage && labels.derivedDamage.length > 0) {
+                    consumeDesc +=`<br>Damage:`
+                labels.derivedDamage.forEach(damage => {
+                    consumeDesc += `
+                    ${damage.label}
                 `;
+                });
+            }
+            if (system.consume.type == "attribute"){
+                 sonsumeDesc += `<br>Consumes: ${system.consume.amount} ${system.consume.target}`;
+                 } else if (system.consume.type == "hitDice"){
+                 consumeDesc += `<br>Consumes: ${system.consume.amount} ${system.consume.target} Hit Die`;
+                 } else if (system.consume.type == "item" || system.consume.type === "ammo" || system.consume.type === "material"){
+                 const conItem = actor.items.get(system.consume.target);
+                 consumeDesc += `<br>Consumes: ${system.consume.amount} ${conItem.name} ${system.consume.type}`;
+                 }
+            if (labels.properties && labels.properties.length > 0) {
+                    consumeDesc +=`<br>Properties: `
+                labels.properties.forEach((prop, index) => {
+                    if (index > 0) {
+                    consumeDesc += ', ';
+                    }
+                    consumeDesc += `${prop.label}`;
                 });
             }
         }
 
+    //SPELLS
         let spellDesc = '';
         if (itemType === 'spell') {
+          if (system.level >= 0 && system.level <= 9) {
             spellDesc = `
-            <h2>[[/item ${itemName}]]</h2>
-            ${desc}
-            Activation: ${item.labels.activation}
-        `;
+            <span style="font-family: 'Modesto Condensed'; font-size: 2em;">${itemName} ${system.uses.per ? ` - Limited Uses: ${system.uses.max} per ${system.uses.per}` : ``}
+</span>
+            <br>Activation: ${labels.activation}
+            `;
+            if (labels.properties && labels.properties.length > 0) {
+                    spellDesc += `<br>`;
+                labels.properties.forEach((prop, index) => {
+                    if (index > 0 && index < labels.properties.length -1) {
+                    spellDesc += ', ';
+                    }
+                    if (prop.label == 'Concentration') {
+                        spellDesc += `${prop.label}<img src="${prop.icon}" width ="30" height="30" style="border: none; float:right; filter:brightness(0) saturate(100%)">`;
+                    } else if (prop.label =='Ritual') {
+                        spellDesc += `${prop.label}<img src="${prop.icon}" width ="30" height="30" style="border: none; float:right; filter:brightness(0) saturate(100%)">`;
+                    } else if (prop.label =='Magical') {
+                        spellDesc += ``;
+                    } else {
+                        spellDesc += `${prop.label}`;
+                    };
+                });
+            }
+            
+            spellDesc+=`
+            ${labels.materials ? `<br>Materials: ${labels.materials}` : ``}
+            ${labels.range ? `<br>Range: ${labels.range}` : ``}
+            ${labels.target ? `<br>Target: ${labels.target}` : ``}
+            ${labels.duration ? `<br>Duration: ${labels.duration}` : ``}
+            ${labels.save ? `<br>Saving Throw: ${labels.save}` : ``}
+            ${labels.toHit ? `<br>Spell Attack: ${labels.toHit}` : ``}
+            ${desc}`
+            if (system.consume.type == "attribute"){
+                 spellDesc += `<br>Consumes: ${system.consume.amount} ${system.consume.target}`;
+                 } else if (system.consume.type == "hitDice"){
+                 spellDesc += `<br>Consumes: ${system.consume.amount} ${system.consume.target} Hit Die`;
+                 } else if (system.consume.type == "item" || system.consume.type === "ammo" || system.consume.type === "material"){
+                 const conItem = actor.items.get(system.consume.target);
+                 spellDesc += `<br>Consumes: ${system.consume.amount} ${conItem.name} ${system.consume.type}`;
+                 }
+               }
         }
 
         const outroContent = `</article></div>`;
@@ -219,13 +314,19 @@ for (const actor of actorFolder.contents) {
             fullContent += weaponDesc;
         } else if (itemType === 'spell') {
             fullContent += spellDesc;
+        } else if (itemType === 'consumable'){
+            fullContent += consumeDesc;
         }
         fullContent += outroContent;
 
         if (itemType === 'weapon') {
             itemContent.weapon.push(fullContent);
-        } else if (itemType === 'spell') {
-            itemContent.spell.push(fullContent);
+        } else  if (itemType === 'spell' && item.system.preparation.mode === 'pact') {
+            itemContent.spellPact.push(fullContent);
+        } else  if (itemType === 'spell' && item.system.level >= 0 && item.system.level <= 9) {
+            itemContent.spell[item.system.level].push(fullContent);
+        } else if (itemType === 'consumable'){
+            itemContent.consumable.push(fullContent);
         }
     });
 
@@ -234,26 +335,52 @@ for (const actor of actorFolder.contents) {
         const column2 = items.slice(Math.ceil(items.length / 2)).join('');
         return `
         <div class="column" style="float: left; width: 50%">
-            <h2>${title}</h2>
             ${column1}
         </div>
         <div class="column" style="float: right; width: 50%">
-            <h2>${title}</h2>
             ${column2}
         </div>
     `;
     }
 
     // Page Content
-    const pageTitle = `${actor.name}`;
-    const weaponsContent = generateColumns('Items - Weapons', itemContent.weapon);
-    const spellsContent = generateColumns('Items - Spells', itemContent.spell);
-    const content = `
-   ${actorContent}
-   <div class="item-columns">
-       ${weaponsContent}
-       <div style="clear:both"></div>>
-       ${spellsContent}
+const pageTitle = `${actor.name}`;
+const weaponsContent = generateColumns('Items - Weapons', itemContent.weapon);
+const pactContent = generateColumns('Pact', itemContent.spellPact)
+const consumeContent = generateColumns('Items - Consumables', itemContent.consumable);
+
+let content = `
+    ${actorContent}
+    <div class="item-columns">
+        <h2>Weapons</h2>
+        ${weaponsContent}
+        <div style="clear:both"></div>
+        <h2>Consumables</h2>
+        ${consumeContent}
+        <div style="clear:both"></div>
+`;
+
+itemContent.spell.forEach((spellLevel, index) => {
+    const spellLevelContent = generateColumns(`Spells ${index} Level`, spellLevel);
+    if (index == 0 ){
+        content += `
+        ${itemContent.spell[0] != "" ? `<h2>Cantrips</h2>
+        ${spellLevelContent}
+        <div style="clear:both"></div>` : ""}
+        ${itemContent.spellPact != "" ? `<h2>Pact Spells Level ${slots.pact.level}</h2>
+        ${pactContent}
+        <div style="clear:both"></div>` : ""}
+    `;
+    } else if (spellLevel != ""){
+    content += `
+        <h2>Spells ${index} Level</h2>
+        ${spellLevelContent}
+        <div style="clear:both"></div>
+    `;
+    }
+});
+       
+       content +=`
    </div>
 `;
 
