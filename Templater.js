a623c6702a0b7e6e60157e1b27918a11583e9923
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2024-07-25 15:31:48 -0700

    Update Templater.js

--- Templater.js
+++ Templater.js
@@ -4,21 +4,31 @@
 //Example: "/m Templater distance=40 width=10" - 40ft long 10ft wide Ray, ie Gust of Wind
 
 const data = {
-    "t": scope.type ? scope.type : "ray",
-    "x": scope.x ? scope.x : 0,
-    "y": scope.y ? scope.y : 0,
-    "distance": scope.distance ? scope.distance : 0,
-    "direction": scope.direction ? scope.direction : 0,
-    "angle": scope.type === "cone" ? 53.2 : 0,
-    "width": scope.width ? scope.width : 5,
-    "borderColor": scope.borderColor ? scope.borderColor : "#000000",
-    "fillColor": scope.fillColor ? scope.fillColor : game.user.color,
-    "hidden": scope.hidden ? scope.hidden : "false",
+    t: scope.type || "ray",
+    x: scope.x || 0,
+    y: scope.y || 0,
+    distance: scope.distance || 0,
+    direction: scope.direction || 0,
+    angle: scope.angle || 53.2,
+    width: scope.width || 5,
+    borderColor: scope.borderColor || "#000000",
+    fillColor: scope.fillColor || game.user.color,
+    "hidden": scope.hidden || "false",
     "flags": {}
-}
-const temp = new MeasuredTemplateDocument(data, {parent: canvas.scene});
-if(scope.x && scope.y) {
-    await MeasuredTemplateDocument.create(temp, {parent: canvas.scene});
-} else {
-    new game.dnd5e.canvas.AbilityTemplate(temp).drawPreview()
 };
+
+const temp = new MeasuredTemplateDocument(data, { parent: canvas.scene });
+
+if (scope.x && scope.y) {
+    await MeasuredTemplateDocument.create(temp, { parent: canvas.scene });
+} else {
+    const windows = Object.values(ui.windows);
+    const maximizedWindows = windows.filter(w => !w._minimized);
+    const minimizedWindows = windows.filter(w => w._minimized);
+    maximizedWindows.forEach(w => w.minimize(true));
+
+    new game.dnd5e.canvas.AbilityTemplate(temp).drawPreview()
+        .then(() => {
+            maximizedWindows.forEach(w => w.maximize(true));
+        })
+}
