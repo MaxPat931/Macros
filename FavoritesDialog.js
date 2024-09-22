ce28b250c3e88ca0c6185614d66fa55f43ef4613
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2024-09-22 00:25:04 -0700

    Create FavoritesDialog.js

---
+++ FavoritesDialog.js
@@ -0,0 +1,37 @@
+// Foundry v12, dnd5e 3.0+
+const tokenActor = _token.actor;
+const spells = tokenActor.system.favorites;
+
+const sortedSpells = spells.sort((a, b) => a.sort - b.sort);
+
+async function getSpellLabel(spell) {
+    const fullItem = await fromUuid(`${_token.actor.uuid}${spell.id}`);
+    return `<span class="item-tooltip item " data-favorite-id="${spell.id}" data-item-id="${spell.id}" draggable="true" data-tooltip="
+        <section class=&quot;loading&quot; data-uuid=${_token.actor.uuid}${spell.id}><i class=&quot;fas fa-spinner fa-spin-pulse&quot;></i></section>
+      " data-tooltip-class="dnd5e2 dnd5e-tooltip item-tooltip" data-tooltip-direction="LEFT">
+      <img src="${fullItem.img}" height="30" style="vertical-align: middle; margin-right: 5px;"> ${fullItem.name} ${fullItem.system.uses?.value ? `(${fullItem.system.uses?.value}/${fullItem.system.uses?.max})` : ""}
+      </span>
+      `;
+}
+
+const buttons = await Promise.all(sortedSpells.map(async (s) => {
+    const label = await getSpellLabel(s);
+    return {
+        label, 
+        action: s.id, 
+        callback: () => { return s.id; }
+    };
+}));
+
+const spellId = await foundry.applications.api.DialogV2.wait({
+    buttons,
+    window: {
+        title: "Favorite Items",
+        icon: "fa-solid fa-star"
+    },
+    render: (event, html) => html.querySelector(".form-footer").style.flexDirection = "column",
+    close: () => { return false; }
+}, { id: "warcaster-dialog", width: 'auto' });
+
+const item = await fromUuid(`${_token.actor.uuid}${spellId}`);
+item.use({ legacy: false });
