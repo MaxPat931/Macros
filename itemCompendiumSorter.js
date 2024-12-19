955e9d1425debcffdb4619916a295e4aac4aa135
Zhell <zhellqol@gmail.com>
2024-12-19 10:52:31 +0100

    Update item compendium sorter.

--- itemCompendiumSorter.js
+++ itemCompendiumSorter.js
@@ -1,90 +1,71 @@
 const key = "dnd5e.items";
 const pack = game.packs.get(key);
 const wasLocked = pack.locked;
+const Cls = pack.documentClass;
 
-async function itemCompendiumFolders(method) {
-  if (wasLocked) await pack.configure({ locked: false });
+const type = await foundry.applications.api.DialogV2.wait({
+  window: {
+    icon: "fas fa-atlas",
+    title: "Item Compendium Folders",
+  },
+  position: {
+    width: 400,
+    height: "auto",
+  },
+  content: "<p>Sort item compendium by type or by rarity?</p>",
+  rejectClose: false,
+  buttons: [
+    {action: "type", label: "Type"},
+    {action: "rarity", label: "Rarity"},
+  ],
+});
+if (!type) return;
 
-  let indexFields;
-  if (method == "Type") {
-    indexFields = "type";
-  } else if (method == "Rarity") {
-    indexFields = "system.rarity";
+if (wasLocked) await pack.configure({locked: false});
+
+const fields = [
+  type === "type" ? "type" : null,
+  type === "rarity" ? "system.rarity" : null,
+].filter(_ => _);
+const index = await pack.getIndex({fields: fields});
+
+// Remove old folders.
+await getDocumentClass("Folder").deleteDocuments(pack.folders.map(f => f.id), {pack: key});
+
+// Map documents to new folders, created dynamically.
+const folders = new Map();
+const updates = [];
+const size = index.size;
+const notif = ui.notifications.info("Foldering the compendium...", {progress: true, pct: 0});
+
+for (const [i, idx] of Array.from(index.values()).entries()) {
+  const path = fields[0];
+  const value = foundry.utils.getProperty(idx, path);
+  let label;
+  switch (type) {
+    case "type":
+      label = game.i18n.localize(CONFIG[Cls.documentName].typeLabels[value]);
+      break;
+    case "rarity":
+      label = CONFIG.DND5E.itemRarity[value]?.capitalize() ?? "Mundane";
+      break;
+    default:
+      continue;
   }
 
-  const index = await pack.getIndex({ fields: [indexFields] });
-
-  const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-
-  const uniqueTypes = [...new Set(index.map(entry => {
-    const nestedFields = indexFields.split('.');
-    let nestedValue = entry;
-    for (const field of nestedFields) {
-      nestedValue = nestedValue[field];
-    }
-    return nestedValue;
-  }))];
-
-  const folderData = uniqueTypes.map(v => ({
-    name: capitalizeFirstLetter(v) || "Mundane",
-    type: "Item"
-  }));
-  const folders = await Folder.createDocuments(folderData, { pack: key });
-
-  const updates = [];
-  for (const idx of index) {
-    const folderName = idx[indexFields] || "Mundane";
-    const folder = folders.find(e => e.name === capitalizeFirstLetter(folderName));
-    updates.push({ _id: idx._id, folder: folder.id });
+  if (!folders.get(label)) {
+    const folder = await getDocumentClass("Folder").create({
+      name: label,
+      type: Cls.documentName,
+    }, {pack: key});
+    folders.set(label, folder);
   }
-  await Item.updateDocuments(updates, { pack: key });
-  if (wasLocked) await pack.configure({ locked: true });
-  await ui.notifications.warn(`${pack.metadata.label} ${method} Folders Created`)
+  const folder = folders.get(label);
+  updates.push({_id: idx._id, folder: folder.id});
+  if (game.release.generation > 12) notif.update({pct: i / size});
 }
+await Cls.updateDocuments(updates, {pack: key});
+if (wasLocked) await pack.configure({locked: true});
 
-async function clearCompendiumFolders() {
-  if (wasLocked) await pack.configure({ locked: false }); //Unlock the compendium if it was locked
-  if (pack.folders.size > 0) {
-    const index = await pack.getIndex(); //get all documents in the pack
-    const updates = [];
-    for (const idx of index) {
-      updates.push({ _id: idx._id, folder: null });
-    }
-    await Item.updateDocuments(updates, { pack: key }); //remove folder data from docs *CHANGE* Item to your comp's type
-
-    const packFolders = pack.folders //grab all the folders
-    const folderIds = packFolders.map(folder => folder.id); //get an array of the ids
-    await Folder.deleteDocuments(folderIds, { pack: key }); //nuke the folders
-    await ui.notifications.warn(`${pack.metadata.label} Folders Cleared`) //completed notification!
-  };
-  if (wasLocked) await pack.configure({ locked: true }); //relock the compendium if it was previously locked
-}
-
-new Dialog({
-  title: "Item Compendium Folders",
-  content: `<h1> Sort Item Compendium by Type or by Rarity?</h1>`,
-  buttons: {
-    level: {
-      label: "Type",
-      callback: async () => {
-        await clearCompendiumFolders();
-        await itemCompendiumFolders("Type");
-      }
-    },
-    school: {
-      label: "Rarity",
-      callback: async () => {
-        await clearCompendiumFolders();
-        await itemCompendiumFolders("Rarity");
-      }
-    },
-    clear: {
-      label: "Clear Folders",
-      callback: async () => {
-        await clearCompendiumFolders();
-      }
-    }
-  }
-}).render(true);
+if (game.release.generation > 12) notif.update({message: "Foldering complete!", pct: 1});
+else ui.notifications.info("Foldering complete!");
