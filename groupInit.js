6a0e1784d798d01ae2dc074431bd15129975e605
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2025-02-25 10:44:01 -0800

    semicolons

--- groupInit.js
+++ groupInit.js
@@ -1,11 +1,11 @@
 // Use with a selected group of tokens.
 // Rolls initiative for one of those tokens, the sets that inititive value for all selected tokends
-const tokenGroup = canvas.tokens.controlled
-const roller = tokenGroup[0]
+const tokenGroup = canvas.tokens.controlled;
+const roller = tokenGroup[0];
 await roller.document.toggleCombatant();
-await game.combat.rollNPC({ messageOptions: { rollMode: CONST.DICE_ROLL_MODES.PRIVATE }})
-const initVal = roller.combatant.initiative
+await game.combat.rollNPC({ messageOptions: { rollMode: CONST.DICE_ROLL_MODES.PRIVATE }});
+const initVal = roller.combatant.initiative;
 for(const token of tokenGroup.slice(1)){
     await token.document.toggleCombatant();
     await token.combatant.update({initiative: initVal});
-  }
+  };
