3ee59ae1d4815f63120ebc23ab52478aa0c0e8a5
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2025-02-25 10:29:31 -0800

    Update groupInit.js
    simplified by Michael

--- groupInit.js
+++ groupInit.js
@@ -1,15 +1,11 @@
 // Use with a selected group of tokens.
 // Rolls initiative for one of those tokens, the sets that inititive value for all selected tokends
 const tokenGroup = canvas.tokens.controlled
-const roller = tokenGroup[1]
+const roller = tokenGroup[0]
 await roller.document.toggleCombatant();
 await game.combat.rollNPC({ messageOptions: { rollMode: CONST.DICE_ROLL_MODES.PRIVATE }})
-
 const initVal = roller.combatant.initiative
-console.log(initVal)
-await roller.document.toggleCombatant();
-
-for(const token of canvas.tokens.controlled){
+for(const token of tokenGroup.slice(1)){
     await token.document.toggleCombatant();
     await token.combatant.update({initiative: initVal});
   }
