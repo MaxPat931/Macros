2ef069eef326abde61efd0b569ca68d78bf0de8c
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2025-03-02 12:03:58 -0800

    Update WS-FFFFautomation.js
    remove xp awards

--- FM/WS-FFFFautomation.js
+++ FM/WS-FFFFautomation.js
@@ -303,9 +303,9 @@ Hooks.on("deleteCombat", async (combat) => {
 
   if (topGiants.length > 1) {
       const topGiantNames = topGiants.map(([giant]) => giant).join(", and ");
-      messageContent += `<p><strong>${topGiantNames}</strong> plucked the most trees with <strong>${topCount}</strong> earning</p><p>[[/award 5xp]]</p>`;
+      messageContent += `<p><strong>${topGiantNames}</strong> plucked the most trees with <strong>${topCount}</strong> earning 5 points!</p>`;
   } else {
-      messageContent += `<p><strong>${topGiants[0][0]}</strong> plucked the most trees with <strong>${topCount}</strong> earning [[/award 5xp]]</p>`;
+      messageContent += `<p><strong>${topGiants[0][0]}</strong> plucked the most trees with <strong>${topCount}</strong> earning 5 points!</p>`;
   }
 
   sortedGiantCount.forEach(([giant, count]) => {
