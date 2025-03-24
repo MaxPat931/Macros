fd549aa0ef1daea1650bd6ca4dd25cc221b56125
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2025-03-24 15:18:45 -0700

    Group Reassignment
    At the start of a new round, two or more bands of minions can reorganize into one band, acting on the lowest initiative count of the bands that combined.

--- groupInit.js
+++ groupInit.js
@@ -1,11 +1,20 @@
 // Use with a selected group of tokens.
-// Rolls initiative for one of those tokens, the sets that inititive value for all selected tokends
 const tokenGroup = canvas.tokens.controlled;
 const roller = tokenGroup[0];
-await roller.document.toggleCombatant();
-await game.combat.rollNPC({ messageOptions: { rollMode: CONST.DICE_ROLL_MODES.PRIVATE }});
-const initVal = roller.combatant.initiative;
-for(const token of tokenGroup.slice(1)){
-    await token.document.toggleCombatant();
-    await token.combatant.update({initiative: initVal});
-  };
+//If the first selected token is not in itiative, roll init and add remaining selected tokens to that init value
+if (!roller.combatant) {
+    await roller.document.toggleCombatant();
+    await game.combat.rollNPC({ messageOptions: { rollMode: CONST.DICE_ROLL_MODES.PRIVATE } });
+    const initVal = roller.combatant.initiative;
+    for (const token of tokenGroup.slice(1)) {
+        if (!token.combatant) await token.document.toggleCombatant();
+        await token.combatant.update({ initiative: initVal });
+    }
+//If the first selected token is in itiative, then add/reassign remaining selected tokens to that init value
+} else {
+    const initVal = roller.combatant.initiative;
+    for (const token of tokenGroup.slice(1)) {
+        if (!token.combatant) await token.document.toggleCombatant();
+        await token.combatant.update({ initiative: initVal });
+    }
+};
