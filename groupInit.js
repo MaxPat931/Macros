9259be6078285e774f8b43470cf2eb7b6c548842
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2025-03-24 17:13:36 -0700

    Active Effect
    Adds an Active Effect with randomized colors to visualize the token groups

--- groupInit.js
+++ groupInit.js
@@ -18,3 +18,28 @@ if (!roller.combatant) {
         await token.combatant.update({ initiative: initVal });
     }
 };
+//Adds/updates an Active Effect on the token to visually indicate groups
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+const effectName = "Token Group";
+const rollerEffect = roller.actor.effects.find(e => e.name === effectName);
+const color = rollerEffect ? rollerEffect.tint : getRandomColor();
+const effectData = {
+    name: effectName,
+    icon: "icons/svg/combat.svg",
+    tint: color,
+    duration: {
+        startTime: game.time.worldTime,
+        rounds: 10
+    }
+};
+
+for (const token of tokenGroup) {
+    const existingEffect = token.actor.effects.find(e => e.label === effectName);
+    if (existingEffect) {
+        existingEffect.update({ tint: color });
+    } else {
+        token.actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+    }
+}
